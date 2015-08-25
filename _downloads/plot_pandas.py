7615d5a43242f8aa0199d72f45ded4a2d257c2a5
Gael Varoquaux <gael.varoquaux@normalesup.org>
2015-08-25 20:12:23 +0200

    Make install

--- _downloads/plot_pandas.py
+++ _downloads/plot_pandas.py
@@ -1,32 +1,28 @@
 """
-Plotting quantities from a CSV file
-=====================================
+Plotting simple quantities of a pandas dataframe
+=================================================
 
 This example loads from a CSV file data with mixed numerical and
 categorical entries, and plots a few quantities, separately for females
-and males, thanks to the pandas integrating plotting tool (that uses
+and males, thanks to the pandas integrated plotting tool (that uses
 matplotlib behind the scene).
 
+See http://pandas.pydata.org/pandas-docs/stable/visualization.html
 """
 
 import pandas
 
-import matplotlib.pyplot as plt
-
 data = pandas.read_csv('brain_size.csv', sep=';', na_values='.')
-gender_data = data.groupby('Gender')
-gender_data.boxplot(column=['FSIQ', 'VIQ', 'PIQ'])
+
+# Box plots of different columns for each gender
+groupby_gender = data.groupby('Gender')
+groupby_gender.boxplot(column=['FSIQ', 'VIQ', 'PIQ'])
 
 from pandas.tools import plotting
-plotting.scatter_matrix(data[['Weight', 'Height', 'MRI_Count']])
 
+# Scatter matrices for different columns
+plotting.scatter_matrix(data[['Weight', 'Height', 'MRI_Count']])
 plotting.scatter_matrix(data[['PIQ', 'VIQ', 'FSIQ']])
 
-plt.figure(figsize=(4, 3))
-data.boxplot(column=['FSIQ', 'PIQ'])
-
-plt.figure(figsize=(4, 3))
-plt.boxplot(data['FSIQ'] - data['PIQ'])
-plt.xticks((1, ), ('FSIQ - PIQ', ))
-
+import matplotlib.pyplot as plt
 plt.show()
