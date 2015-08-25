0a907fff75d80a317585a23f82e73be312e434ad
Gael Varoquaux <gael.varoquaux@normalesup.org>
2015-08-25 20:10:44 +0200

    Better example description

--- examples/plot_paired_boxplots.py
+++ examples/plot_paired_boxplots.py
@@ -2,6 +2,12 @@
 Boxplots and paired differences
 =================================================
 
+Plot boxplots for FSIQ, PIQ, and the paired difference between the two:
+while the spread (error bars) for FSIQ and PIQ are very large, there is a
+systematic (common) effect due to the subjects. This effect is cancelled
+out in the difference and the spread of the difference ("paired" by
+subject) is much smaller than the spread of the individual measures.
+
 """
 
 import pandas
